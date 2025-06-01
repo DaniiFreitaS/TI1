d57fb9fd28afc9443d8bec6e2063c6d8b0b08c3d
Daniel Felipe Coelho de Freitas <danielfreitaslele14@gmail.com>
2025-06-01 19:36:50 -0300

    att

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject painelPausa;
+    private bool jogoPausado = false;
+
+    void Start()
+    {
+        painelPausa.SetActive(false); // desativa no início
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (jogoPausado)
+                Retomar();
+            else
+                Pausar();
+        }
+    }
+
+    public void Pausar()
+    {
+        painelPausa.SetActive(true);
+        Time.timeScale = 0f;
+        jogoPausado = true;
+    }
+
+    public void Retomar()
+    {
+        painelPausa.SetActive(false);
+        Time.timeScale = 1f;
+        jogoPausado = false;
+    }
+
+    public void VoltarMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MenuInicial"); // ajuste para o nome da sua cena de menu
+    }
+}
